f24e133cfa227a1a4897004d590f9efac33ec83b
aragamix01 <aragamix01@gmail.com>
2018-03-29 04:50:12 +0700

    add physical

--- getObjectLeftKey.php
+++ getObjectLeftKey.php
@@ -2,7 +2,7 @@
     include_once("./connect.php");
     $conn = getConnection();
 
-    $sql = "SELECT mo.obj_id, mo.obj_title, mok.keyword_obj_id, mok.keyword FROM muse_object AS mo
+    $sql = "SELECT mo.obj_id, mo.obj_title, mok.keyword_obj_id, mok.keyword , mo.obj_physicals FROM muse_object AS mo
             LEFT JOIN muse_object_keyword AS mok ON mo.obj_id = mok.obj_id
             ORDER BY mo.obj_id";
 
@@ -26,18 +26,18 @@
                 }
                 if ($obj->keyword_obj_id == null) {
                     if($oldId != null) {
-                        array_push($dataTable,array('id' => $oldId, 'name' => $old, 'key' => $ref));
+                        array_push($dataTable,array('id' => $oldId, 'name' => $old, 'physical' => $oldPhy, 'key' => $ref));
                         $ref = array();
                         $old = null;
                         $oldKey = null;
                         $oldId = null;
                     } 
-                    array_push($dataTable, array('id' => $obj->obj_id, 'name' => $obj->obj_title, 'key' => array()));
+                    array_push($dataTable, array('id' => $obj->obj_id, 'name' => $obj->obj_title, 'physical' => $obj->obj_physicals, 'key' => array()));
                     $check = -1;
                 } else {
                     if ($obj->obj_id != $check && $check = -1) {
                         if ($oldId != null)
-                            array_push($dataTable,array('id' => $oldId, 'name' => $old, 'key' => $ref));
+                            array_push($dataTable,array('id' => $oldId, 'name' => $old, 'physical' => $oldPhy, 'key' => $ref));
                         $check = $obj->obj_id;
                         $ref = array();
                         array_push($ref,$obj->keyword);
@@ -47,6 +47,7 @@
                         $old = $obj->obj_title;
                         $oldKey = $obj->keyword_obj_id;
                         $oldId = $obj->obj_id;
+                        $oldPhy = $obj->obj_physicals;
                     }
                 }
                 $i++;
